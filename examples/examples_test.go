efd178c23282f2fa4e3ec7c0d20ea87523a7ddbb
stack72 <public@paulstack.co.uk>
2020-06-03 14:46:42 +0100

    Migrate to GitHub Actions

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -2,58 +2,16 @@ package examples
 
 import (
 	"os"
-	"path"
-	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
-func TestAccTeamTs(t *testing.T) {
-	test := getJSBaseOptions(t).
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "team", "ts"),
-		})
-
-	integration.ProgramTest(t, &test)
-}
-
-func TestAccTeamPython(t *testing.T) {
-	test := getPythonBaseOptions(t).
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "team", "python"),
-		})
-
-	integration.ProgramTest(t, &test)
-}
 
 func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	checkPagerdutyToken(t)
 	return integration.ProgramTestOptions{
 		ExpectRefreshChanges: true,
 	}
-}
-
-func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
-	base := getBaseOptions(t)
-	baseJS := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-			"@pulumi/pagerduty",
-		},
-	})
-
-	return baseJS
-}
-
-func getPythonBaseOptions(t *testing.T) integration.ProgramTestOptions {
-	base := getBaseOptions(t)
-	basePy := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-			filepath.Join("..", "sdk", "python", "bin"),
-		},
-	})
-
-	return basePy
 }
 
 func checkPagerdutyToken(t *testing.T) {
